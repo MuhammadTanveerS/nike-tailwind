afe99b6d2da610df071bf8568c76efd8b2d8ee4d
MuhammadTanveerS <muhammadtanveer.bcsf18@iba-suk.edu.pk>
2023-09-12 12:49:37 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import Nav from './components/Nav';
 import { CustomerReviews, Hero, PopularProducts, Services, SpecialOffer, Subscribe, SuperQuality, Footer } from './sections'
 
 const App = () => (
   <main className="relative">
-    {/* <Nav /> */}
+    <Nav />
     <section className="xl:padding-1 wide:padding-r padding-b">
       <Hero />
     </section>
